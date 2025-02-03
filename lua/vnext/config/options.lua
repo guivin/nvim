1480adeca6c3a0ff5cb7f30edbc8a15cc80f6d3f
Guillaume Vincent <23073977+guivin@users.noreply.github.com>
2025-02-03 19:43:27 +0100

    feat: update initial forked code
    - Comment disable default mappings to declutter which-key (not-working in my case)
    - Add lua/vnext/extra/custom.lua
    - Disable allaman/emoji.nvim plugin
    - Disable allaman/kustomize.nvim plugin
    - Remove lua/vnext/extra/example.lua
    - Add new settings in lua/vnext/config/options.lua

--- lua/vnext/config/options.lua
+++ lua/vnext/config/options.lua
@@ -18,8 +18,14 @@ o.listchars = {                                                    -- define inv
   space = "·",
   nbsp = "␣",
 }
+o.hidden         = true
+o.encoding       = "UTF-8"
+o.backspace      = "indent,eol,start"
+o.incsearch      = true
+o.hlsearch       = false
 o.mouse          = "nv"                                            -- enable mouse see :h mouse
-o.relativenumber = true                                            -- set relative numbered lines
+o.number         = true
+o.relativenumber = false                                           -- set relative numbered lines
 o.scrolloff      = 10                                              -- Minimal number of screen lines to keep above and below the cursor
 o.signcolumn     = "yes"                                           -- Always show the signcolumn, otherwise it would shift the text each time
 o.shiftwidth     = 2                                               -- the number of spaces inserted for each indentation
